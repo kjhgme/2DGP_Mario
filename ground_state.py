95e587032e6e41f8049a5285b9f77a18e08be68a
kjhgme <kjhgme@naver.com>
2021-11-15 02:36:05 +0900

    game_framework 추가

---
+++ ground_state.py
@@ -0,0 +1,26 @@
+import game_framework
+import pico2d
+
+name = "GroundState"
+image = None
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
--- mario.py
+++ mario.py
@@ -1,133 +1,62 @@
 from pico2d import *
+import select_map_state
 
-stance = 0
-num = 0
-x = 20
-y = 90
-class Backgrond:
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, UP_DOWN, UP_UP, SPACE = range(7)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_UP): UP_DOWN,
+    (SDL_KEYUP, SDLK_UP): UP_UP,
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+}
+
+class IdleState:
+    pass
+
+class RunState:
+    pass
+
+class JumpState:
+    pass
+
+class FireballState:
+    pass
+
+next_state_table = {
+
+}
+
+class Mario:
     def __init__(self):
-        self.image = load_image('image/background.png')
-
-    def draw(self):
-        self.image.draw(0, 400, 2666, 728)
-
-class Small_mario:
-    global dir
-    global num
-    def __init__(self):
-        self.x, self.y = 20, 90
-        self.image = load_image('image/mario/smallmario/marioR/stand.png')
-        self.frame = 0
-
-    def update(self):
         pass
 
-    def draw(self):
-        self.image.clip_draw(self.frame, 0, 50, 80, x, y)
+    def change_state(self, state):
+        pass
 
-class Gumba:
-    def __init__(self):
-        self.x, self.y = 700, 80
-        self.image = load_image('image/monster/monsterL/0.png')
-        self.frame = 0
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+        pass
 
     def update(self):
-        self.frame = (self.frame + 1) % 5
-        self.image = load_image('image/monster/monsterL/'+str(self.frame)+'.png')
-        self.x -= 2
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
     def draw(self):
-        self.image.clip_draw(0, 0, 50, 60, self.x, self.y)
+        # fill here
+        self.cur_state.draw(self)
+        debug_print('Velocity : ' + str(self.velocity) + ' Dir : ' + str(self.dir) + 'State : ' + str(self.cur_state.__name__))
+        pass
 
-class Turtle:
-    def __init__(self):
-        self.x, self.y = 500, 80
-        self.image = load_image('image/tortoise/tortoiseL/0.png')
-        self.frame = 0
-
-    def update(self):
-        self.frame = (self.frame + 1) % 12
-        self.image = load_image('image/tortoise/tortoiseL/' + str(self.frame) + '.png')
-        self.x -= 2
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 55, 70, self.x, self.y)
-
-def Handle_events():
-    global running
-    global x
-    global y
-    global dir
-    global stance
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir += 1
-                stance = 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-                stance = 2
-            elif event.key == SDLK_UP:
-                y += 10
-                stance = 3
-            elif event.key == SDLK_UP:
-                y += 10
-                stance = 4
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-                stance = 0
-            elif event.key == SDLK_LEFT:
-                dir += 1
-                stance = 5
-            elif event.key == SDLK_UP:
-                y -= 10
-                stance = 0
-
-open_canvas()
-background =Backgrond()
-small_mario = Small_mario()
-gumba = Gumba()
-turtle = Turtle()
-running = True
-dir = 0
-
-def mario_stance():
-    global num
-    if stance == 0:
-        small_mario.image = load_image('image/mario/smallmario/marioR/stand.png')
-    elif stance == 1:
-        num = (num + 1) % 22
-        small_mario.image = load_image('image/mario/smallmario/marioR/' + str(num) + '.png')
-    elif stance == 2:
-        num = (num + 1) % 22
-        small_mario.image = load_image('image/mario/smallmario/marioL/' + str(num) + '.png')
-    elif stance == 3:
-        small_mario.image = load_image('image/mario/smallmario/marioR/jump.png')
-    elif stance == 4:
-        small_mario.image = load_image('image/mario/smallmario/marioL/jump.png')
-    elif stance == 5:
-        small_mario.image = load_image('image/mario/smallmario/marioL/stand.png')
-
-while running:
-    clear_canvas()
-    background.draw()
-    small_mario.draw()
-    gumba.draw()
-    turtle.draw()
-    update_canvas()
-    small_mario.update()
-    gumba.update()
-    turtle.update()
-    Handle_events()
-    x += dir * 5
-    mario_stance()
-    delay(0.01)
-
-close_canvas()
+    def handle_event(self, event):
+        # fill here
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+        pass
