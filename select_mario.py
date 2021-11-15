3ae2a514dee3da8b8dade5c7ca3c592c5fc8099e
kjhgme <kjhgme@naver.com>
2021-11-16 01:58:09 +0900

    select_mario_move

--- select_mario.py
+++ select_mario.py
@@ -8,26 +8,45 @@ class SelectMario:
         self.y = 300
         self.selectX = 0
         self.selectY = 0
+        self.frame = 0
 
     def update(self):
+        self.image = load_image('image/mario/bigmario/marioR/stand.png')
         if self.selectX == 1 and self.x < 600:
             self.x += 1
+            self.frame = (self.frame + 1) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
         elif self.selectX == 0:
             if self.x > 400:
                 self.x -= 1
+                self.frame = (self.frame + 1) % 22
+                self.image = load_image('image/mario/bigmario/marioL/' + str(self.frame) + '.png')
             elif self.x < 400:
                 self.x += 1
+                self.frame = (self.frame + 1) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
         elif self.selectX == -1 and self.x > 200:
             self.x -= 1
+            self.frame = (self.frame + 1) % 22
+            self.image = load_image('image/mario/bigmario/marioL/' + str(self.frame) + '.png')
         if self.selectY == 1 and self.y < 450:
             self.y += 1
+            self.frame = (self.frame + 1) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
         elif self.selectY == 0:
             if self.y > 300:
                 self.y -= 1
+                self.frame = (self.frame + 1) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
             elif self.y < 300:
                 self.y += 1
+                self.frame = (self.frame + 1) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
         elif self.selectY == -1 and self.y > 150:
             self.y -= 1
+            self.frame = (self.frame + 1) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+
 
     def draw(self):
         self.image.clip_draw(0, 0, 70, 90, self.x, self.y)
