d15fa53c80cb6f4bcc3246b2080dbd8e10bcb820
kjhgme <kjhgme@naver.com>
2021-11-16 02:27:55 +0900

    select_mario_move_time

--- select_mario.py
+++ select_mario.py
@@ -1,6 +1,17 @@
 from pico2d import *
 import game_framework
 
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class SelectMario:
     def __init__(self):
         self.image = load_image('image/mario/bigmario/marioR/stand.png')
@@ -13,39 +24,39 @@ class SelectMario:
     def update(self):
         self.image = load_image('image/mario/bigmario/marioR/stand.png')
         if self.selectX == 1 and self.x < 600:
-            self.x += 1
-            self.frame = (self.frame + 1) % 22
-            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+            self.x += int(game_framework.frame_time * RUN_SPEED_PPS)+1
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
         elif self.selectX == 0:
             if self.x > 400:
-                self.x -= 1
-                self.frame = (self.frame + 1) % 22
-                self.image = load_image('image/mario/bigmario/marioL/' + str(self.frame) + '.png')
+                self.x -= int(game_framework.frame_time * RUN_SPEED_PPS)+1
+                self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+                self.image = load_image('image/mario/bigmario/marioL/' + str(int(self.frame)) + '.png')
             elif self.x < 400:
-                self.x += 1
-                self.frame = (self.frame + 1) % 22
-                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+                self.x += int(game_framework.frame_time * RUN_SPEED_PPS)+1
+                self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
         elif self.selectX == -1 and self.x > 200:
-            self.x -= 1
-            self.frame = (self.frame + 1) % 22
-            self.image = load_image('image/mario/bigmario/marioL/' + str(self.frame) + '.png')
+            self.x -= int(game_framework.frame_time * RUN_SPEED_PPS)+1
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+            self.image = load_image('image/mario/bigmario/marioL/' + str(int(self.frame)) + '.png')
         if self.selectY == 1 and self.y < 450:
-            self.y += 1
-            self.frame = (self.frame + 1) % 22
-            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+            self.y += int(game_framework.frame_time * RUN_SPEED_PPS)+1
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
         elif self.selectY == 0:
             if self.y > 300:
-                self.y -= 1
-                self.frame = (self.frame + 1) % 22
-                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+                self.y -= int(game_framework.frame_time * RUN_SPEED_PPS)+1
+                self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
             elif self.y < 300:
-                self.y += 1
-                self.frame = (self.frame + 1) % 22
-                self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+                self.y += int(game_framework.frame_time * RUN_SPEED_PPS)+1
+                self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+                self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
         elif self.selectY == -1 and self.y > 150:
-            self.y -= 1
-            self.frame = (self.frame + 1) % 22
-            self.image = load_image('image/mario/bigmario/marioR/' + str(self.frame) + '.png')
+            self.y -= int(game_framework.frame_time * RUN_SPEED_PPS)+1
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 22
+            self.image = load_image('image/mario/bigmario/marioR/' + str(int(self.frame)) + '.png')
 
 
     def draw(self):
